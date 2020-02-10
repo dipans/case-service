46ea55867ab1cfe6170e18644a56e0c9a98c4807
dipans <dipan.cs@gmail.com>
2020-02-09 23:16:20 -0500

    Jenkis CI/CD integration

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 SQLALCHEMY_ECHO = True
 SQLALCHEMY_TRACK_MODIFICATIONS = True
-SQLALCHEMY_DATABASE_URI = "postgresql://postgres:password@postgres:5432/case_service"
+SQLALCHEMY_DATABASE_URI = "postgresql://postgres:password@postgres:5432/caseservice"
 
 
 #SQLALCHEMY_DATABASE_URI = "sqlite:///db.sqlite3"
